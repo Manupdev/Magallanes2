14eb418740b31c9597451e6084dcc8341ec4ab3f
Santiago Canovari <santiago.canovari@avature.net>
2022-11-29 16:43:55 -0300

    added allowed host for testing

--- Magallanes/settings.py
+++ Magallanes/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1o4#t@sj*$40!k-g5%(@-k7wy@9wyi@ng#@1rm@uttr_62rkd9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['santiagocanovari.pythonanywhere.com']
 
 
 # Application definition
